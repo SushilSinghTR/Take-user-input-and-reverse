1b91220a1ded21a76a7a4c7246a9dbe7855dddeb
SushilSinghTR <88138899+SushilSinghTR@users.noreply.github.com>
2022-04-19 14:02:13 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+// // let name= document.getElementById("name");
+// // console.log(name);
+
+// function submithandler(){
+//     let name= document.getElementById("name");
+// //    let res= name.split('').reverse().join()
+//     console.log(res);
+
+// }
+var formData = new FormData(document.querySelector('form'))
